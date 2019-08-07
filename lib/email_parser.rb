855f1633509474e354ac2d3088caa19fed76f95c
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2019-08-07 03:58:35 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,7 +6,7 @@ class EmailAddressParser
 
   attr_accessor :email_addresses
 
-  def initalize(email_addresses)
+  def initialize(email_addresses)
     @email_addresses = email_addresses
     #parse
   end
