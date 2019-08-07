84a7d5a04b0f931202c7f3ab3d4d6305639f0b1b
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2019-08-07 04:01:03 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,7 +13,7 @@ class EmailAddressParser
 
   def parse
       if @email_addresses.include?(", ") then
-        @email_addresses.sub(", "," ")
+        @email_addresses.sub!(", "," ")
       end
 
       @email_addresses.split(" ")
